c1ddf2d264bb1b1a2cb29b164338c6326a3f1242
Barnuss09 <hollbarna@gmail.com>
2020-04-20 17:50:11 +0200

    navigation

--- pokemon/src/modules/NavBar.js
+++ pokemon/src/modules/NavBar.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <div>
       <header>
-        <h2>Pokemon App</h2>
+        <h1>Pokemon App</h1>
+        <Link to="/">Pokemons</Link> | <Link to="/types">Types</Link>
       </header>
     </div>
   );
